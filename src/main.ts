ff766737b79981f2b0a1e3ce4a9e73d940604d0d
iraneldf <iranel831@gmail.com>
2025-01-22 00:31:07 -0500

    Cambio de puerto

--- src/main.ts
+++ src/main.ts
@@ -27,9 +27,8 @@ async function bootstrap() {
 
   app.enableCors()
 
-  await app.listen(3000);
-  // await app.listen(process.env.PORT ?? 3000);
-
+  // await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap()
